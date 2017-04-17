1d77a9c76000cb08c88df30dddc7f4adb9df4527
jamie <jamie951123@gmail.com>
2017-04-18 03:21:56 +0800

    again

---
+++ src/main/java/com/jamie/rms/searchcriteria/object/ProductSearchObject.java
@@ -0,0 +1,22 @@
+package com.jamie.rms.searchcriteria.object;
+
+public class ProductSearchObject {
+	private String partyId;
+
+	public String getPartyId() {
+		return partyId;
+	}
+
+	public void setPartyId(String partyId) {
+		this.partyId = partyId;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductSearchObject [partyId=" + partyId + "]";
+	}
+
+	
+	
+	
+}
