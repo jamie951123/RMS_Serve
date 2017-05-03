c6cc164f6aa88211ee482580bd1e8c3b937ab770
jamie <jamie951123@gmail.com>
2017-05-04 02:56:01 +0800

    tomcat not finish

--- src/main/java/com/jamie/rms/Application.java
+++ src/main/java/com/jamie/rms/Application.java
@@ -1,14 +1,25 @@
 package com.jamie.rms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
-import com.jamie.rms.controller.LoginController;
 
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
+	 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
 }
