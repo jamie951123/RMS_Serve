149bed61ee7643df64f9720afded92350a309fae
jamie <jamie951123@gmail.com>
2017-06-05 23:28:13 +0800

    start the serve
    remain : gradle and tomcatport

--- src/main/java/com/jamie/rms/Application.java
+++ src/main/java/com/jamie/rms/Application.java
@@ -1,11 +1,16 @@
 package com.jamie.rms;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @ComponentScan
@@ -13,6 +18,9 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
+	 @Autowired
+	    private ServletContext servletContext;
+	 
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(Application.class);
@@ -20,6 +28,18 @@ public class Application extends SpringBootServletInitializer{
 	 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @PostConstruct
+    public void showIt() {
+        System.out.println("Serve Base Path :"+servletContext.getContextPath());
+    }
+    
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("Hello World");
+		return "Hello";
+
     }
     
 }
