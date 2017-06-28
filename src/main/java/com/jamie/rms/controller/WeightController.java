4d57026a836b992652fd5872dce78c36ea5c71e6
jamie951123 <jamie951123@gmail.com>
2017-06-28 15:14:34 +0800

    fk

--- src/main/java/com/jamie/rms/controller/WeightController.java
+++ src/main/java/com/jamie/rms/controller/WeightController.java
@@ -74,14 +74,14 @@ public class WeightController {
 		}
 		log.info("[WeightProfile]-[delete]-User Request(GSON) : "+ weightProfile);
 		
-//		try{
-//		//remove (FK) Product
-//			productController.updateWeightIdNullByPartyIdAndWeightId(weightProfile_json);
-//		}catch (Exception e){
-//			e.printStackTrace();
-//			log.warn("[WeightProfile]-[Error]-delete : weightProfile is empty");
-//			throw e;
-//		}
+		try{
+		//remove (FK) Product
+			productController.updateWeightIdNullByPartyIdAndWeightId(weightProfile_json);
+		}catch (Exception e){
+			e.printStackTrace();
+			log.warn("[WeightProfile]-[Error]-delete : weightProfile is empty");
+			throw e;
+		}
 		//hibernate remove (FK) OnetoMany Product
 		//remove Weight
 		try{
